ae7fe898f7c3b69215ad5d582d95f3ef6241b23b
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-14 14:16:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/projectmanager/trunk@117899 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/core.php
+++ lib/core.php
@@ -737,7 +737,6 @@ class ProjectManager extends ProjectManagerLoader
 		
 		$datasets = $wpdb->get_results($sql);
 		
-		echo $options['project_options'][$this->project_id]['dataset_orderby'];
 		if ( $options['project_options'][$this->project_id]['dataset_orderby'] == 'formfields' || $formfield_id )
 			$datasets = $this->orderDatasetsByFormFields($datasets, $formfield_id);
 		
@@ -796,6 +795,7 @@ class ProjectManager extends ProjectManagerLoader
 			$to_sort[] = $form_field_id;
 		}
 	
+		print_r($to_sort);
 		/*
 		* Only process datasets if there is anything to do
 		*/
