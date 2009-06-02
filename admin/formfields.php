db3dcb9f2a8e389fe66dd5b3ee6a277f9fb4a7c7
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-02 22:50:45 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/projectmanager/tags/2.5@122450 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/formfields.php
+++ admin/formfields.php
@@ -1,5 +1,5 @@
 <?php
-if ( !current_user_can( 'projectmanager_admin' ) ) : 
+if ( !current_user_can( 'edit_formfields' ) ) : 
      echo '<p style="text-align: center;">'.__("You do not have sufficient permissions to access this page.").'</p>';
 else :
 
