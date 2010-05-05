1ef18e4aded875e16e2992ce0db6dc44d59b5dfa
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-05 08:35:19 +0000

    fixed slideshow conflict with NextGen Gallery
    git-svn-id: https://plugins.svn.wordpress.org/projectmanager/tags/2.9.6@237654 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin.php
+++ admin/admin.php
@@ -82,8 +82,9 @@ class ProjectManagerAdminPanel extends ProjectManager
 		// Add global Projects Menu
 		add_menu_page(__('Projects', 'projectmanager'), __('Projects', 'projectmanager'), 'view_projects', PROJECTMANAGER_PATH,array(&$this, 'display'), PROJECTMANAGER_URL.'/admin/icons/menu/databases.png');
 
-		add_submenu_page(PROJECTMANAGER_PATH, __('Projects', 'projectmanager'), __('Overview','projectmanager'),'view_projects', PROJECTMANAGER_PATH,array(&$this, 'display'));
+		add_submenu_page(PROJECTMANAGER_PATH, __('Projects', 'projectmanager'), __('Overview','projectmanager'),'view_projects', PROJECTMANAGER_PATH, array(&$this, 'display'));
 		add_submenu_page(PROJECTMANAGER_PATH, __( 'Settings'), __('Settings'), 'projectmanager_settings', 'projectmanager-settings', array( &$this, 'display') );
+//		add_submenu_page(PROJECTMANAGER_PATH, __( 'Documentation', 'projectmanager'), __('Documentation', 'projectmanager'), 'view_projects', 'projectmanager-documentation', array( &$this, 'display') );
 		
 		$plugin = 'projectmanager/projectmanager.php';
 		add_filter( 'plugin_action_links_' . $plugin, array( &$this, 'pluginActions' ) );
@@ -117,6 +118,9 @@ class ProjectManagerAdminPanel extends ProjectManager
 			case 'projectmanager-settings':
 				$this->displayOptionsPage();
 				break;
+			case 'projectmanager-documentation':
+			  include_once( dirname(__FILE__) . '/documentation.php' );
+			  break;
 			case 'projectmanager':
 				switch($_GET['subpage']) {
 					case 'show-project':
@@ -424,7 +428,11 @@ class ProjectManagerAdminPanel extends ProjectManager
 		}
 
 		$wpdb->query( $wpdb->prepare( "INSERT INTO {$wpdb->projectmanager_projects} (title) VALUES ('%s')", $title ) );
+		$project_id = $wpdb->insert_id;
+		
 		$this->setMessage( __('Project added','projectmanager') );
+		
+		do_action('projectmanager_add_project', $project_id);
 	}
 	
 	
@@ -446,6 +454,8 @@ class ProjectManagerAdminPanel extends ProjectManager
 		
 		$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->projectmanager_projects} SET `title` = '%s' WHERE `id` = '%d'", $title, $project_id ) );
 		$this->setMessage( __('Project updated','projectmanager') );
+		
+		do_action('projectmanager_edit_project', $project_id);
 	}
 	
 	
@@ -468,6 +478,8 @@ class ProjectManagerAdminPanel extends ProjectManager
 		
 		$wpdb->query( "DELETE FROM {$wpdb->projectmanager_projectmeta} WHERE `project_id` = {$project_id}" );
 		$wpdb->query( "DELETE FROM {$wpdb->projectmanager_projects} WHERE `id` = {$project_id}" );
+		
+		do_action('projectmanager_del_project', $project_id);
 	}
 
 	
@@ -489,6 +501,8 @@ class ProjectManagerAdminPanel extends ProjectManager
 
 		$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->projectmanager_projects} SET `settings` = '%s' WHERE `id` = '%d'", maybe_serialize($settings), $project_id ) );
 		$this->setMessage(__('Settings saved', 'projectmanager'));
+		
+		do_action('projectmanager_save_settings', $project_id);
 	}
 
 
@@ -519,20 +533,35 @@ class ProjectManagerAdminPanel extends ProjectManager
 				if ($handle) {
 					if ( "TAB" == $delimiter ) $delimiter = "\t"; // correct tabular delimiter
 					
-					$i = 0; // initialize dataset counter
+					$i = 0; $l=0; // initialize dataset & line counter
 					while (!feof($handle)) {
-						$buffer = fgets($handle, 4096);
-						$line = explode($delimiter, $buffer);
-						$name = $line[0];
-						// assign column values to form fields
-						foreach ( $cols AS $col => $form_field_id ) {
-							$meta[$form_field_id] = $line[$col];
-						}
-						
-						if ( $line && !empty($name) ) {
-							$this->addDataset($project_id, $name, array(), $meta);
-							$i++;
-						}
+						  $buffer = fgets($handle, 4096);
+						  $line = explode($delimiter, $buffer);
+						  
+						  if ( $l > 0 && $line ) {
+  						  $name = $line[0];
+  						  $categories = empty($line[1]) ? '' : explode(",", $line[1]);
+                /*
+    						* get Category IDs from titles
+    						*/						
+    						$cat_ids = array();
+    						if ( !empty($categories) ) {
+    						  foreach ( $categories AS $category ) {
+    						    $cat_ids[] = get_cat_ID($category);
+                 				  }
+                				}
+                
+    						// assign column values to form fields
+    						foreach ( $cols AS $col => $form_field_id ) {
+    							$meta[$form_field_id] = $line[$col];
+    						}
+    						
+    						if ( $line && !empty($name) ) {
+    							$this->addDataset($project_id, $name, $cat_ids, $meta);
+    							$i++;
+    						}
+  					  }
+  					  $l++;
 					}
 					fclose($handle);
 					
@@ -580,10 +609,10 @@ class ProjectManagerAdminPanel extends ProjectManager
 	{
 		global $projectmanager;
 		
-		if ( !current_user_can('import_datasets') ) {
-			$this->setMessage( __("You don't have permission to perform this task", 'projectmanager'), true );
-			return;
-		}
+		//if ( !current_user_can('import_datasets') ) {
+		//	$this->setMessage( __("You don't have permission to perform this task", 'projectmanager'), true );
+		//	return;
+		//}
 
 		$this->project_id = $project_id;
 		$projectmanager->initialize($project_id);
@@ -603,12 +632,12 @@ class ProjectManagerAdminPanel extends ProjectManager
 			foreach ( $projectmanager->getDatasetMeta( $dataset->id ) AS $meta ) {
 				// Remove line breaks
 				$meta->value = str_replace("\r\n", "", stripslashes($meta->value));
-				$contents .= "\t".$meta->value;
+				$contents .= "\t".strip_tags($meta->value);
 			}
 		}
 		
 		header('Content-Type: text/csv');
-    		header('Content-Disposition: inline; filename="'.$filename.'"');
+		header('Content-Disposition: inline; filename="'.$filename.'"');
 		echo $contents;
 		exit();
 	}
@@ -633,7 +662,6 @@ class ProjectManagerAdminPanel extends ProjectManager
 			return false;
 		}
 
-		
 		$projectmanager->initialize($project_id);
 		$this->project_id = $project_id;
 		$project = $this->project = $projectmanager->getProject($project_id);
@@ -653,7 +681,6 @@ class ProjectManagerAdminPanel extends ProjectManager
 				return;
 			}
 		}
-
 
 		$wpdb->query( $wpdb->prepare( "INSERT INTO {$wpdb->projectmanager_dataset} (name, cat_ids, project_id, user_id) VALUES ('%s', '%s', '%d', '%d')", $name, maybe_serialize($cat_ids), $project_id, $user_id ) );
 		$dataset_id = $wpdb->insert_id;
@@ -690,8 +717,11 @@ class ProjectManagerAdminPanel extends ProjectManager
 					
 				if ( is_array($meta_value) ) {
 					// form field value is a date
-					if ( array_key_exists('day', $meta_value) && array_key_exists('month', $meta_value) && array_key_exists('year', $meta_value) )
-						$meta_value = $meta_value['year'].'-'.str_pad($meta_value['month'], 2, 0, STR_PAD_LEFT).'-'.str_pad($meta_value['day'], 2, 0, STR_PAD_LEFT);
+					if ( array_key_exists('day', $meta_value) && array_key_exists('month', $meta_value) && array_key_exists('year', $meta_value) ) {
+						$meta_value = sprintf("%s-%s-%s", $meta_value['year'], $meta_value['month'], $meta_value['day']);
+					} elseif ( array_key_exists('hour', $meta_value) && array_key_exists('minute', $meta_value) ) {
+						$meta_value = sprintf("%s:%s", $meta_value['hour'], $meta_value['minute']);
+					}
 				}
 
 				$wpdb->query( $wpdb->prepare( "INSERT INTO {$wpdb->projectmanager_datasetmeta} (form_id, dataset_id, value) VALUES ('%d', '%d', '%s')", $meta_id, $dataset_id, maybe_serialize($meta_value) ) );
@@ -717,6 +747,8 @@ class ProjectManagerAdminPanel extends ProjectManager
 			$this->uploadImage($dataset_id, $_FILES['projectmanager_image']);
 				
 		$this->setMessage( __( 'New dataset added to the database.', 'projectmanager' ) );
+		
+		do_action('projectmanager_add_dataset', $dataset_id);
 	}
 		
 		
@@ -792,8 +824,11 @@ class ProjectManagerAdminPanel extends ProjectManager
 					
 				if ( is_array($meta_value) ) {
 					// form field value is a date
-					if ( array_key_exists('day', $meta_value) && array_key_exists('month', $meta_value) && array_key_exists('year', $meta_value) )
-						$meta_value = $meta_value['year'].'-'.str_pad($meta_value['month'], 2, 0, STR_PAD_LEFT).'-'.str_pad($meta_value['day'], 2, 0, STR_PAD_LEFT);
+					if ( array_key_exists('day', $meta_value) && array_key_exists('month', $meta_value) && array_key_exists('year', $meta_value) ) {
+						$meta_value = sprintf("%s-%s-%s", $meta_value['year'], $meta_value['month'], $meta_value['day']);
+					} elseif ( array_key_exists('hour', $meta_value) && array_key_exists('minute', $meta_value) ) {
+						$meta_value = sprintf("%s:%s", $meta_value['hour'], $meta_value['minute']);
+					}
 				}
 					
 				if ( 1 == $wpdb->get_var( "SELECT COUNT(ID) FROM {$wpdb->projectmanager_datasetmeta} WHERE `dataset_id` = '".$dataset_id."' AND `form_id` = '".$meta_id."'" ) )
@@ -836,9 +871,36 @@ class ProjectManagerAdminPanel extends ProjectManager
 		}
 			
 		$this->setmessage( __('Dataset updated.', 'projectmanager') );
+		
+		do_action('projectmanager_edit_dataset', $dataset_id);
 	}
 		
-		
+	
+  /**
+   * duplicate dataset
+   * 
+   * @param int $dataset_id
+   * @return boolean
+   */
+  function duplicateDataset( $dataset_id )
+  {
+    global $projectmanager, $wpdb;
+    $dataset = $projectmanager->getDataset( $dataset_id );
+    $meta = $projectmanager->getDatasetMeta( $dataset_id );
+    
+    $meta_data = array();
+    foreach ( $meta AS $m ) {
+      $meta_data[$m->form_field_id] = $m->value;
+    }
+    
+    $this->addDataset($dataset->project_id, $dataset->name, maybe_unserialize($dataset->cat_ids), $meta_data);
+    $id = $wpdb->insert_id;
+    $wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->projectmanager_dataset} SET `image` = '%s' WHERE id = '%d'", $dataset->image, $id ) );
+    
+    return true;
+  }
+  
+                	
 	/**
 	 * delete dataset
 	 *
@@ -867,6 +929,8 @@ class ProjectManagerAdminPanel extends ProjectManager
 		}
 		$wpdb->query("DELETE FROM {$wpdb->projectmanager_datasetmeta} WHERE `dataset_id` = {$dataset_id}");
 		$wpdb->query("DELETE FROM {$wpdb->projectmanager_dataset} WHERE `id` = {$dataset_id}");
+		
+		do_action('projectmanager_del_dataset', $dataset_id);
 	}
 	
 	
@@ -1032,7 +1096,7 @@ class ProjectManagerAdminPanel extends ProjectManager
 				}
 				
 				$wpdb->query( $wpdb->prepare( "INSERT INTO {$wpdb->projectmanager_projectmeta} (`label`, `type`, `show_on_startpage`, `show_in_profile`, `order`, `order_by`, `project_id`) VALUES ( '%s', '%s', '%d', '%d', '%d', '%d', '%d');", $formfield['name'], $formfield['type'], $show_on_startpage, $show_in_profile, $order, $order_by, $project_id ) );
-				$id = mysql_insert_id();
+				$id = $wpdb->insert_id;
 					
 				// Redirect form field options to correct $form_id if present
 				if ( isset($options['form_field_options'][$tmp_id]) ) {
@@ -1051,8 +1115,13 @@ class ProjectManagerAdminPanel extends ProjectManager
 			}
 		}
 		
+		if ( isset($options['form_field_options'][$id]) )
+		  sort($options['form_field_options'][$id]);
+		  
 		update_option('projectmanager', $options);
 		$this->setMessage( __('Form Fields updated', 'projectmanager') );
+		
+		do_action('projectmanager_save_formfields', $dataset_id);
 	}
 	
 	
@@ -1129,6 +1198,7 @@ class ProjectManagerAdminPanel extends ProjectManager
 					echo '<h3>'.$projectmanager->getProjectTitle().'</h3>';
 					echo '<input type="hidden" name="project_id['.$dataset_id.']" value="'.$project_id.'" /><input type="hidden" name="dataset_id[]" value="'.$dataset_id.'" /><input type="hidden" name="dataset_user_id" value="'.$current_user->ID.'" />';
 				
+				  $projectmanager->loadTinyMCE();
 					include( dirname(__FILE__). '/dataset-form-profile.php' );
 				}
 			}
@@ -1150,26 +1220,6 @@ class ProjectManagerAdminPanel extends ProjectManager
 			$del_image = isset( $_POST['del_old_image'][$id] ) ? true : false;
 			$overwrite_image = ( isset($_POST['overwrite_image'][$id]) && 1 == $_POST['overwrite_image'][$id] ) ? true: false;
 			$this->editDataset( $_POST['project_id'][$id], $_POST['display_name'], $_POST['post_category'][$id], $id, $_POST['form_field'][$id], $user_id, $del_image, $_POST['image_file'][$id], $overwrite_image );
-		}
-	}
-
-
-	/**
-	 * registrer new user
-	 *
-	 * @param int $user_id
-	 * @return void
-	 */
-	function registerUser( $user_id )
-	{
-		global $projectmanager;
-
-		$user = new WP_User($user_id);
-		if ( $user->has_cap('projectmanager_user') ) {
-			foreach ( $projectmanager->getProjects() AS $project ) {
-				if ( 1 == $project->profile_hook ) 
-					$this->addDataset( $project->id, $user->first_name, array(), false, $user_id );
-			}
 		}
 	}
 }
